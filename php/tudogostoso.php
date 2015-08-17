48bd5db7bc7e2692deb1437688fae4c7e8af0f16
Matheus Baptista Bondan <matheus_1111@hotmail.com>
2015-08-17 14:16:56 -0300

    json

--- php/tudogostoso.php
+++ php/tudogostoso.php
@@ -8,31 +8,45 @@ class tudogostoso {
         foreach ($array as &$value) {
             $html = file_get_html($value);
 
-            $image = "imagem";
-
-            $name = $html->find("span.current",0);
-            echo $name;
-
-            $recipelist = $html->find('div.recipelist', 0);  
-            echo $recipelist;
-
-            $IntructionsRecipelist = $html->find('*[class="recipelist instructions"]', 0);
-            echo $IntructionsRecipelist;
+            echo '<br />';
             
-            $rendimento = $html->find("span.label",1);
-            echo $rendimento;
+            $imageurl = $html->find('*[class="pic"]', 0);
+            $image = $imageurl->getAttribute('src');
+            echo $image;
 
-            $porcao = $html->find('*[class="num yield"]',0);
-            echo $porcao;
+             echo '<br />';
+            
+            $name1 = $html->find("span.current",0);
+            $name = \strip_tags($name1);
+            echo ($name1);
+            
 
-            $tempPreparoTitle = $html->find("span.label",0);
-            echo $tempPreparoTitle;
+            $recipelist1 = $html->find('div.recipelist', 0);
+            $recipelist = \strip_tags($recipelist1);
+            echo $recipelist1;
 
-            $tempPreparo = $html->find('*[class="num preptime"]',0);
-            echo $tempPreparo;
+            $IntructionsRecipelist1 = $html->find('*[class="recipelist instructions"]', 0);
+            $IntructionsRecipelist = \strip_tags($IntructionsRecipelist1);
+            echo $IntructionsRecipelist1;
+            
+            $rendimento1 = $html->find("span.label",1);
+            $rendimento = \strip_tags($rendimento1);
+            echo $rendimento1;
+
+            $porcao1 = $html->find('*[class="num yield"]',0);
+            $porcao = \strip_tags($porcao1);
+            echo $porcao1;
+
+            $tempPreparoTitle1 = $html->find("span.label",0);
+            $tempPreparoTitle = \strip_tags($tempPreparoTitle1);
+            echo $tempPreparoTitle1;
+
+            $tempPreparo1 = $html->find('*[class="num preptime"]',0);
+            $tempPreparo = \strip_tags($tempPreparo1);
+            echo $tempPreparo1;
             
             $site="www.tudogostoso.com.br";
-            $receita = new Receita($name,$image,$recipelist,$IntructionsRecipelist,$rendimento,$tempPreparo,$site);
+            $receita = new Receita($name,$image,$recipelist,$IntructionsRecipelist,$porcao,$tempPreparo,$site);
             $connection = new Connection();
             //$connection->Connect();
             $connection->save($receita);   
